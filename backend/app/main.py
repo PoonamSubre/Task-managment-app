d7bb2f8350bb21acef84920e694e3f84b5627d0f
Poonam Subre <poonamsubre@gmail.com>
2024-07-18 01:15:24 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # CORS middleware to allow requests from localhost:3000
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://task-managment-app-weld.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
