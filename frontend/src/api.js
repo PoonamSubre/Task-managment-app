1d3e5d7c5ecafb34277b5be450eafd4059f5d915
Poonam Subre <poonamsubre@gmail.com>
2024-07-18 01:52:38 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,23 +1,23 @@
 import axios from 'axios';
 
-const API_URL = 'https://task-managment-app-19.onrender.com';
+const API_URL = 'https://task-managment-app-19.onrender.com/api';
 
 export const getTasks = async () => {
-  const response = await axios.get(API_URL);
+  const response = await axios.get(`${API_URL}/tasks`);
   return response.data;
 };
 
 export const createTask = async (task) => {
-  const response = await axios.post(API_URL, task);
+  const response = await axios.post(`${API_URL}/tasks`, task);
   return response.data;
 };
 
 export const updateTaskStatus = async (taskId, status) => {
-  const response = await axios.put(`${API_URL}/${taskId}`, { status });
+  const response = await axios.put(`${API_URL}/tasks/${taskId}`, { status });
   return response.data;
 };
 
 export const deleteTask = async (taskId) => {
-  const response = await axios.delete(`${API_URL}/${taskId}`);
+  const response = await axios.delete(`${API_URL}/tasks/${taskId}`);
   return response.data;
 };
