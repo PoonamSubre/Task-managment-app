921c097e5d7923e53bfb654c0d6986b08c9dc15c
Poonam Subre <poonamsubre@gmail.com>
2024-07-17 23:51:15 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -9,7 +9,7 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-=======
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
