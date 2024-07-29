569c662fc8fb42e828eb00c6a1f28d14d7524562
Poonam Subre <poonamsubre@gmail.com>
2024-07-29 17:22:29 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://task-managment-app-19.onrender.com/api';
+const API_URL = 'https://task-managment-app-19.onrender.com/api/tasks';
 
 export const getTasks = async () => {
   try {
